716c58dd9f1bba644abdcdfc90bb828778566e0f
denvermullets <devmullets@gmail.com>
2023-05-13 10:18:25 -0400

    feat(lineups): build out lineup scrape service

--- app/controllers/api/v1/ingest_stats_controller.rb
+++ app/controllers/api/v1/ingest_stats_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class IngestStatsController < ApplicationController
       def index
-        Ingest::Lineups.call
+        Ingest::Lineups.call(year: 2022, team: 'PHO')
 
         render json: { hi: 'hi' }
       end
--- app/services/ingest/lineups.rb
+++ app/services/ingest/lineups.rb
@@ -1,12 +1,65 @@
+require 'net/http'
+
 module Ingest
   class Lineups < Service
-    def initialize
-      # initialize stuff
+    def initialize(year:, team:)
+      @url = "https://www.basketball-reference.com/teams/#{team.upcase}/#{year}_start.html"
+      @games = []
     end
 
     def call
-      # do stuff
-      puts 'hello'
+      doc = load_html
+      parse_html(doc)
+
+      data = { games: @games }
+
+      json = JSON.generate(data)
+      File.write('data.json', json)
+      puts 'Scraping completed!'
+    end
+
+    def load_html
+      # url = URI.parse('https://www.basketball-reference.com/teams/PHO/2022_start.html')
+      url = URI.parse(@url)
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = (url.scheme == 'https')
+
+      request = Net::HTTP::Get.new(url.request_uri)
+      response = http.request(request)
+      html = response.body
+
+      Nokogiri::HTML(html)
+    end
+
+    def parse_html(doc)
+      doc.css('tbody tr').each do |row|
+        date = row.css('[data-stat="date_game"] a').text
+        opponent = row.css('[data-stat="opp_name"] a').text
+        win_status = row.css('[data-stat="game_result"]').text
+        team_points = row.css('[data-stat="pts"]').text
+        opponent_points = row.css('[data-stat="opp_pts"]').text
+        win = row.css('[data-stat="wins"]').text
+        loss = row.css('[data-stat="losses"]').text
+
+        starting_lineup = row.css('[data-stat="game_starters"] a').map do |a|
+          player = a.text
+          player_url = a['href']
+          { player:, player_url: }
+        end
+
+        @games << {
+          date:,
+          opponent:,
+          game_won: win_status == 'W',
+          team_points:,
+          opponent_points:,
+          win:,
+          loss:,
+          startingLineup: {
+            players: starting_lineup
+          }
+        }
+      end
     end
   end
 end

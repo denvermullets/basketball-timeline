eeffc2a954b50b7ad489053e802a08827370ba20
denvermullets <devmullets@gmail.com>
2023-05-25 08:08:57 -0400

    fix(teams): fix out of order team list

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class TeamsController < ApplicationController
       def index
-        render json: Team.all, except: %i[created_at updated_at]
+        render json: Team.all.order(:name), except: %i[created_at updated_at]
       end
     end
   end
